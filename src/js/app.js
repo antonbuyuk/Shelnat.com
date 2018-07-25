4b8106d3193cc490d71dabc225f664be7a9536d1
antonbuyuk <anton.buyuk@abiatec.com>
2018-07-25 18:28:33 +0300

    main_page

--- src/js/app.js
+++ src/js/app.js
@@ -5,20 +5,44 @@ $(document).ready(function(){
 
     $('.burger').click(function () {
         $(this).toggleClass('is-active');
-    })
+        $('.menu_area').toggleClass('is-active');
+    });    
 })
+
 
 // fullpage
 $(document).ready(function () {
     $('#fullpage').fullpage({
         //options here
-        menu: '#menu',
-        anchors: ['main', 'about', 'how', 'app', 'team', 'backUp', 'contact'],
+        navigation: true,
+        anchors:['main', 'portfolio', 'design-project', 'services', 'articles', 'video', 'education', 'faq', 'contacts'],
+        navigationPosition: 'right',
+        navigationTooltips: ['main', 'portfolio', 'design-project', 'services', 'articles', 'video', 'education', 'faq', 'contacts'],
+        showActiveTooltip: false,
         autoScrolling: true,
         scrollHorizontally: true,
+        scrollingSpeed: 400,
         fadingEffect: true,
         // responsiveWidth: 640,
-        licenseKey: "OPEN-SOURCE-GPLV3-LICENSE"
+        licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+
+        onLeave: function(origin, destination, direction){
+            var leavingSection = this;
+    
+            //после покидания раздела 2
+            if(destination.index == 3){
+                $('.header').removeClass('header-black');
+                $('.header').addClass('header-white');
+            }
+    
+            else if(destination.index == 7){
+                $('.header').removeClass('header-black');
+                $('.header').addClass('header-white');
+            } else {
+                $('.header').removeClass('header-white');
+                $('.header').addClass('header-black');
+            }
+        }
     });
 });
 
