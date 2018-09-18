7839c65f5edbee62e63f13750eb5bdaacf6b60f5
Anton Buyuk <anton.buyuk@abiatec.by>
2018-09-18 22:10:56 +0300

    obile + filters

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,48 @@
 $(document).foundation();
 
 $(document).ready(function(){
+    course();
     $('select').styler();
+
+    if ($('#fullpage').length) {
+        $('#default_page').attr('id', 'main_page');
+    } else if ($('.week').length) {
+        $('#default_page').attr('id', 'courses_page');
+    }
+
+    $('.article_info .article_text').each(function () {
+        var text = $(this).text();
+        more = '...';
+
+        $(this).html(text + more);
+    });
+
+    $('.course_info .course_text').each(function () {
+        var text = $(this).text();
+        more = '...';
+
+        $(this).html(text + more);
+    })
+
+    if ($('.infinite').length) {
+        $('.infinite').infiniteScroll({
+            // options
+            path: '.next.page-numbers',
+            append: '.infinite_post',
+            history: false,
+        });
+    }
+
+
+    $('.design_card .btn').click(function () {
+        var price = $(this).parents('.design_card').find('.priceCourse').text();
+        $('.totalCash span').parents('.totalCash').fadeIn('slow');
+        $('.totalCash span').text(price);
+
+        console.log($('#selectCourse').val())
+    })
+    
+
 
     $('.burger').click(function () {
         $(this).toggleClass('is-active');
@@ -20,7 +61,6 @@ $(document).ready(function(){
     }
 
     $('.showProject').click(function(){
-        
         setTimeout(function(){ $('.fancybox-container').addClass('fancybox-show-thumb'); }, 1000);
     })
 
@@ -58,10 +98,19 @@ $(document).ready(function(){
             });
         })
     }
+
+    if ($("iframe").length) {
+        $("iframe").each(function () {
+            $(this).parents("p").addClass("iframe_parents")
+        })
+    }
 })
 
 $(window).on("load", function () {
     
+    $(".slider_countainer").mCustomScrollbar({
+        axis: "x"
+    });
 
     if ($(window).width() < '1024') {
         $("#videoList").mCustomScrollbar({
@@ -72,23 +121,42 @@ $(window).on("load", function () {
             axis: "y"
         });
     }
+    
+});
+
+// Slider  
+$(window).on("load", function () {
+
+    $('.slider').each(function () {
+        var imgWidth = +$('.slider_item').width();
+            imgCount = +$('.slider_item').length;
+            areaWidth = imgWidth * imgCount + 30 * imgCount - 0;
+        
+        $(this).find('.slider_area').css('width', areaWidth);
+
+        $(this).find('.slider_arrow-right').click(function () {
+            
+        })
+    })
 });
 
 
 $('.portfolio_carousel').owlCarousel({
     loop: true,
     margin: 30,
-    nav: true,
-    navText: ["<img src='./wp-content/themes/shelnat/templates/_ares/img/svg/prev.svg'>", "<img src='./wp-content/themes/shelnat/templates/_ares/img/svg/next.svg'>"],
+    navText: ["<img src='/wp-content/themes/shelnat/templates/_ares/img/svg/prev.svg'>", "<img src='/wp-content/themes/shelnat/templates/_ares/img/svg/next.svg'>"],
     responsive: {
         0: {
-            items: 1
+            items: 1,
+            nav: false
         },
         640: {
-            items: 2
+            items: 2,
+            nav: true
         },
         1024: {
-            items: 4
+            items: 4,
+            nav: true
         }
     }
 });
@@ -96,17 +164,19 @@ $('.portfolio_carousel').owlCarousel({
 $('.courses_carousel').owlCarousel({
     loop: false,
     margin: 30,
-    nav: true,
-    navText: ["<img src='./wp-content/themes/shelnat/templates/_ares/img/svg/prev.svg'>", "<img src='./wp-content/themes/shelnat/templates/_ares/img/svg/next.svg'>"],
+    navText: ["<img src='/wp-content/themes/shelnat/templates/_ares/img/svg/prev.svg'>", "<img src='/wp-content/themes/shelnat/templates/_ares/img/svg/next.svg'>"],
     responsive: {
         0: {
-            items: 1
+            items: 1,
+            nav: false
         },
         640: {
-            items: 1
+            items: 1,
+            nav: true
         },
         1024: {
-            items: 2
+            items: 2,
+            nav: true,
         }
     }
 });
@@ -115,7 +185,10 @@ $('.carousel_project').owlCarousel({
     loop: true,
     margin: 0,
     nav: false,
-    items: 1
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 4000,
+
 })
 
 // fullpage
@@ -123,9 +196,9 @@ $(document).ready(function () {
     $('#fullpage').fullpage({
         //options here
         navigation: true,
-        anchors: ['main', 'portfolio', 'design-project', 'services', 'articles', 'video',  'faq', 'contacts'],
+        anchors: ['main', 'portfolio', 'design-project', 'services', 'articles', 'video', 'education',  'faq', 'contacts'],
         navigationPosition: 'right',
-        navigationTooltips: ['main', 'portfolio', 'design-project', 'services', 'articles', 'video',  'faq', 'contacts'],
+        navigationTooltips: ['main', 'portfolio', 'design-project', 'services', 'articles', 'video', 'education', 'faq', 'contacts'],
         showActiveTooltip: false,
         autoScrolling: true,
         scrollHorizontally: true,
@@ -141,7 +214,7 @@ $(document).ready(function () {
             if (destination.index == 3) {
                 $('.header').removeClass('header-black');
                 $('.header').addClass('header-white');
-            } else if (destination.index == 6) {
+            } else if (destination.index == 7) {
                 $('.header').removeClass('header-black');
                 $('.header').addClass('header-white');
             } else {
@@ -152,6 +225,31 @@ $(document).ready(function () {
     });
 });
 
-$(window).ready(function () {
-    $('body').css('opacity', '1');
- })
+function course() {
+    $('.design_card').each(function () {
+        var name = $(this).find('.nameCourse').text();
+            price = $(this).find('.priceCourse').text();
+            price = price.replace(/\s+/g, '');
+            intPrice = parseInt(price.substr(0, price.length - 4));
+            
+        $('#selectCourse').append('<option value=' + intPrice + '>' + name + '</option>');
+    })
+}
+
+$('#countOrder').val(randomInteger(1, 999999));
+
+function randomInteger(min, max) {
+    var rand = min - 1 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+}
+
+
+
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('.wrapper').css('opacity', '1');
+    }, 1000);
+    $('.pleloader').fadeOut('slow');
+    $('body').removeClass('preloader_is-active');
+});
